3301a860d1fb2bae6b2a42bf14a859af110a0bab
Kunal Katre <katre.kunal25@gmail.com>
2022-05-05 10:49:50 +0530

    Added Caching to retain timer on page refresh

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ let countdownTitle = '';
 let countdownDate='';
 let countdownValue = Date;
 let countdownActive;
+let savedCountdown;
 
 const second = 1000;
 const minute = second * 60;
@@ -24,19 +25,29 @@ const today = new Date().toISOString().split('T')[0];
 console.log(today);
 dateEl.setAttribute('min', today);
 
+//Store previous countdown
+function retorePreviousCountdown(){
+    if (localStorage.getItem('countdown')){
+        inputCountDownTimer.hidden = true;
+        savedCountdown = JSON.parse(localStorage.getItem('countdown'));
+        countdownTitle = savedCountdown.title;
+        countdownDate = savedCountdown.date;
+        countdownValue = new Date(countdownDate).getTime();
+        updateDOM();
+    }
+}
+
 //Update Countdown 
 function updateDOM(){
    
     countdownActive = setInterval(() => {
         const now = new Date().getTime();
         const distance = countdownValue - now;
-        console.log('distance:', distance);
     
         const days = Math.floor(distance/day);
         const hours = Math.floor((distance % day) / hour);
         const minutes = Math.floor((distance % hour) / minute);
         const seconds = Math.floor((distance % minute) / second);
-        console.log(days, hours, minutes, seconds);
     
         //Hide Input
         inputCountDownTimer.hidden=true;
@@ -83,16 +94,22 @@ function updateCountDown(e){
     e.preventDefault();
     countdownTitle = e.srcElement[0].value;
     countdownDate = e.srcElement[1].value;
-    console.log(countdownTitle, countdownDate);
     
+    savedCountdown = {
+        title : countdownTitle,
+        date : countdownDate,
+    };
+
+    localStorage.setItem('countdown', JSON.stringify(savedCountdown));
+    console.log(savedCountdown);
+
     //Check if input values are valid 
     if (countdownDate === ''){
         alert('Please enter valid values');
     }
     else{
-         //Get number version of Date , update DOM
+        //Get number version of Date , update DOM
         countdownValue = new Date(countdownDate).getTime();
-        console.log('Countdown Value:', countdownValue);
         updateDOM();
     }
 
@@ -102,3 +119,6 @@ function updateCountDown(e){
 countdownForm.addEventListener('submit', updateCountDown);
 countdownBtn.addEventListener('click', reset);
 completeBtn.addEventListener('click', reset);
+
+//Onload, check local storage
+retorePreviousCountdown();
